9ebb013c501ab104669528cfc3f74c226959e941
Attila Babo <attila.babo@gmail.com>
2014-12-17 15:59:19 +0100

    Fixed tests

--- tests/specs/mcash.shortlink_spec.js
+++ tests/specs/mcash.shortlink_spec.js
@@ -33,10 +33,32 @@
 
             mCASH.displayQRorButton();
 
-            var img = $('#a img[src="https://api.mca.sh/shortlink/v1/qr_image/moo/"]');
+            var img = $('#a img[src="https://api.mca.sh/shortlink/v1/qr_image/moo/"]'),
+                button = $('#a button.paywithmcash')
             expect(img.length).toBe(0);
+            expect(button.length).toBe(1);
 
-            console.log($('#a'));
+            spyOn(mCASH, 'redirect_to');
+            button.click();
+            expect(mCASH.redirect_to).toHaveBeenCalledWith("mcash://qr?code=http://mca.sh/s/moo/");
+        });
+
+        it('creates a QR button from an id and an argstring', function () {
+            var userAgent = navigator.userAgent;
+            navigator.__defineGetter__('userAgent', function(){
+                return 'iPad';
+            });
+
+            mCASH.displayQRorButton();
+
+            var img = $('#b img[src="https://api.mca.sh/shortlink/v1/qr_image/moo/"]'),
+                button = $('#b button.paywithmcash')
+            expect(img.length).toBe(0);
+            expect(button.length).toBe(1);
+
+            spyOn(mCASH, 'redirect_to');
+            button.click();
+            expect(mCASH.redirect_to).toHaveBeenCalledWith("mcash://qr?code=http://mca.sh/s/moo/far");
         });
     });
 
